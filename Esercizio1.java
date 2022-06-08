8fb45b523a637d6ece18ee1452074a295366ae4d
Alessandro Ninni <alessandroninni@AirdiAlessandro.fritz.box>
2022-06-08 11:33:28 +0200

    esecuzione esercizio1

---
+++ Esercizio1.java
@@ -0,0 +1,22 @@
+package helloworld;
+
+public class Esercizio1 {
+
+	public static void main(String[] args) {
+		String parola = new String ("anna");
+
+	}
+
+	public static boolean isPalindromo(String parola) {
+		
+       int length = parola.length();
+       int lunghezza;
+       lunghezza = parola.length();
+      double metà= lunghezza/2;
+        
+        if (lunghezza<2)
+        	return true;
+        if ()
+	}
+
+}
